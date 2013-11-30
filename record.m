f6dadddf17dc65624075f50e1551abe92ffb3785
sebastien huwart <sebastienhuwart@gmail.com>
2013-11-30 16:17:11 +0100

    suppression ancien repertoire

--- record.m
+++
@@ -1,6 +0,0 @@
-function [signalRecorder] = record(Fs, duration)
-    % Record
-    signalRecorder = audiorecorder(Fs, 16, 1);
-    disp('Start speaking')
-    recordblocking(signalRecorder, duration);
-end
