0f305f418798e4bcdad19307d8a53354dfa28bb2
sebastien huwart <sebastienhuwart@gmail.com>
2013-12-11 20:15:06 +0100

    Get_url -> fonction qui recoit en paramètres un acronyme et qui ouvre automatiquiement la page wiki de cet acronyme

---
+++ MATLAB_CODE_SOURCE/get_url.m
@@ -0,0 +1,7 @@
+function get_url(acron)
+
+%Ouverture d'une page wiki avec l'acronyme choisi
+acrUp = upper(acron);
+url = 'http://fr.wikipedia.org/wiki/';
+path = strcat(url,acrUp);
+web(path);%ouvre une page de browser avec l'adresse précisé dans le path
